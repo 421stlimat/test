da56ceb5622a68bf27af010d552174e3397086dc
421stlimat <127832637+421stlimat@users.noreply.github.com>
2023-04-28 10:20:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def sigterm_handler(signal, frame):
 signal.signal(signal.SIGTERM, sigterm_handler)
 
 # Start ngrok
-ngrok.set_auth_token('2P0zOs6CcjHOdJC3gVEiFaet5rn_3v6NEFF8L8wax9oYqMCmS')
 tunnel = ngrok.connect(80, 'tcp')
 print(f"ngrok tunnel established at {tunnel.public_url}")
 
