91eb70d11ec57a68b129d13bff7e0b27836a273c
421stlimat <127832637+421stlimat@users.noreply.github.com>
2023-04-28 08:13:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from time import sleep
+from pyngrok import ngrok
+
+ngrok.set_auth_token('2P0zOs6CcjHOdJC3gVEiFaet5rn_3v6NEFF8L8wax9oYqMCmS')
+tunnel =ngrok.connect(80, 'tcp')
 
 while True:
     sleep(10)
