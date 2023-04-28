f565a6ad14d33945c016b7d6df0dd60e25d28dd4
421stlimat <127832637+421stlimat@users.noreply.github.com>
2023-04-28 09:32:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+import signal
 from time import sleep
 from pyngrok import ngrok
 
-ngrok.set_auth_token('2P0zOs6CcjHOdJC3gVEiFaet5rn_3v6NEFF8L8wax9oYqMCmS')
-tunnel =ngrok.connect(80, 'tcp')
+# Define a signal handler to catch the SIGTERM signal
+def sigterm_handler(signal, frame):
+    print("SIGTERM received, stopping ngrok...")
+    ngrok_process = ngrok.get_ngrok_process()
+    if ngrok_process:
+        ngrok.kill()
+    # Perform any necessary cleanup or shutdown tasks here
+    exit(0)
 
+# Register the signal handler with the signal module
+signal.signal(signal.SIGTERM, sigterm_handler)
+
+# Start ngrok
+ngrok.set_auth_token('2P0zOs6CcjHOdJC3gVEiFaet5rn_3v6NEFF8L8wax9oYqMCmS')
+tunnel = ngrok.connect(80, 'tcp')
+print(f"ngrok tunnel established at {tunnel.public_url}")
+
+# Keep the script running indefinitely
 while True:
     sleep(10)
