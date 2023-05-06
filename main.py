34668693cb54b6ab653b5963181de50893b9eafb
421stlimat <127832637+421stlimat@users.noreply.github.com>
2023-05-06 11:27:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ screenshot = ImageGrab.grab()
 
 # Upload the image to FreeImage.host
 url = "https://freeimage.host/api/1/upload"
+headers = {"Authorization": "Bearer 6d207e02198a847aa98d0a2a901485a5"}
 files = {"image": ("screenshot.png", screenshot.tobytes(), "image/png")}
-response = requests.post(url, files=files)
+response = requests.post(url, headers=headers, files=files)
 
 # Print the URL of the uploaded file
 if response.status_code == 200:
