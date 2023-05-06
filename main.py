ea512b402baccec7608fdc196767de63c43be347
421stlimat <127832637+421stlimat@users.noreply.github.com>
2023-05-06 11:33:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import pyautogui
 import requests
-from PIL import ImageGrab
+import base64
 
-# Take a screenshot
-screenshot = ImageGrab.grab()
+# take screenshot
+screenshot = pyautogui.screenshot()
 
-# Upload the image to FreeImage.host
-url = "https://freeimage.host/api/1/upload"
-headers = {"Authorization": "Bearer 6d207e02198a847aa98d0a2a901485a5"}
-files = {"image": ("screenshot.png", screenshot.tobytes(), "image/png")}
-response = requests.post(url, headers=headers, files=files)
+# convert image to base64 string
+img_buffer = io.BytesIO()
+screenshot.save(img_buffer, format='PNG')
+img_str = base64.b64encode(img_buffer.getvalue()).decode('ascii')
 
-# Print the URL of the uploaded file
-if response.status_code == 200:
-    response_json = response.json()
-    print(response_json.get("image").get("url"))
-else:
-    print(response.text)
+# set API endpoint and parameters
+url = 'https://freeimage.host/api/1/upload'
+params = {
+    'key': '6d207e02198a847aa98d0a2a901485a5',
+    'action': 'upload',
+    'source': img_str,
+    'format': 'json'
+}
+
+# send POST request to API
+response = requests.post(url, data=params)
+
+# print response
+print(response.json())
