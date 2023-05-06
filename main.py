3f51ecff43243064c2e8171bc9e96a3b61733953
421stlimat <127832637+421stlimat@users.noreply.github.com>
2023-05-06 11:12:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ image_base64 = base64.b64encode(image_data).decode('utf-8')
 
 # Upload the image to Imgur
 url = 'https://api.imgur.com/3/image'
-headers = {'Authorization': 'Client-ID 7d4122d89e16b07'}
+headers = {'Authorization': 'Client-ID f41f23e0b23c105'}
 payload = {'image': image_base64}
 response = requests.post(url, headers=headers, data=payload)
 
